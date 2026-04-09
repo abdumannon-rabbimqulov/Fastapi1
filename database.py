5c52ca1eaf3c74ef9fb8ed66dd45f13fdc587bd9
Abdumannon Rabbimqulov <abdumannonrab@icloud.com>
2026-04-09 12:57:21 +0500

    bot

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine,AsyncSession,async_sessio
 from sqlalchemy.orm import DeclarativeBase
 from dotenv import load_dotenv
 import os
+from typing import AsyncGenerator
 
 load_dotenv()
 
@@ -10,9 +11,24 @@ Database_URL=os.getenv('Database_URL')
 
 engine=create_async_engine(Database_URL,echo=True)
 
-async_session=async_sessionmaker(
-    engine, expire_on_commit=False,class_=AsyncSession
+async_session = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
 )
 
 class Base(DeclarativeBase):
     pass
+
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
---
+++ products/crud.py
@@ -0,0 +1,11 @@
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+from products.models import Products
+from products.schemas import ProductCreate
+
+async def create_products(db : Session, data : ProductCreate):
+    product=Products(**data.model_dump())
+    db.add(product)
+    db.commit()
+    db.refresh(product)
+    return product
